54b361e71d51ba31cad065e3e3bca162e617dfae
sriprakash-gnanaguru <sriprakashgnanaguru@gmail.com>
2024-03-09 19:55:40 +0100

    initial

---
+++ src/main/java/com/food/recipe/manage/recipemanagement/RecipeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.food.recipe.manage.recipemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementApplication.class, args);
+	}
+
+}
